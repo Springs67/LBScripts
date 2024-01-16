fc9253c9c290f8197a310a798742a11172c8e228
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2024-01-16 15:39:37 -0500

    Update Speed2.js

--- Movement/Speed2.js
+++ Movement/Speed2.js
@@ -1,5 +1,3 @@
-// recoding most speeds so yea. only blocksmc and vulcan work lol
-
 /// api_version=2
 
 var plr = mc.thePlayer
@@ -10,106 +8,164 @@ function setTimer(v) { mc.timer.timerSpeed = v }
 function setYPos(v) { plr.setPosition(plr.posX, plr.posY + v, plr.posZ) }
 var tick = 0
 var tick2 = 0
-var modulesEnabled = 0
-var play_client = "net.minecraft.network.play.client"
+var client = "net.minecraft.network.play.client"
 
 var packets = [
-    C0FPacketConfirmTransaction = Java.type(play_client + ".C0FPacketConfirmTransaction"),
-    C04PacketPlayerPosition = Java.type(play_client + ".C03PacketPlayer.C04PacketPlayerPosition"),
-    C18PacketSpectate = Java.type(play_client + ".C18PacketSpectate"),
-    C03PacketPlayer = Java.type(play_client + ".C03PacketPlayer"),
-    C02PacketUseEntity = Java.type(play_client + ".C02PacketUseEntity"),
-    C00PacketKeepAlive = Java.type(play_client + ".C00PacketKeepAlive"),
-    C13PacketPlayerAbilities = Java.type(play_client + ".C13PacketPlayerAbilities"),
-    C09PacketHeldItemChange = Java.type(play_client + ".C09PacketHeldItemChange"),
-    C17PacketCustomPayload = Java.type(play_client + ".C17PacketCustomPayload"),
+    C0FPacketConfirmTransaction = Java.type(client + ".C0FPacketConfirmTransaction"),
+    C04PacketPlayerPosition = Java.type(client + ".C03PacketPlayer.C04PacketPlayerPosition"),
+    C18PacketSpectate = Java.type(client + ".C18PacketSpectate"),
+    C03PacketPlayer = Java.type(client + ".C03PacketPlayer"),
+    C02PacketUseEntity = Java.type(client + ".C02PacketUseEntity"),
+    C00PacketKeepAlive = Java.type(client + ".C00PacketKeepAlive"),
+    C13PacketPlayerAbilities = Java.type(client + ".C13PacketPlayerAbilities"),
+    C09PacketHeldItemChange = Java.type(client + ".C09PacketHeldItemChange"),
+    C17PacketCustomPayload = Java.type(client + ".C17PacketCustomPayload"),
 ]
-var tick = 0
-var timeInAir = 0
 
-var CustomSpeed = registerScript({
-    name: "Custom Speed",
+function spoofGround() {
+    plr.onGround = true
+    sendPacket(new C03PacketPlayer(true))
+}
+
+var Speed2 = registerScript({
+    name: "Speed2",
     version: "1.0.0",
     authors: ["spring67"]
-});
+})
 
-CustomSpeed.registerModule({
+Speed2.registerModule({
     name: "Speed2",
     category: "Fun",
-    description: "bypasses",
-    tag: "blehhh",
+    description: "bypassing speed",
+    tag: "Custom",
     settings: {
         SpeedMode: Setting.list({
             name: "Mode",
-            default: "UpdatedNCP",
+            default: "Custom",
             values: [
-                "DesyncBoost",
-
+                "TakaLow",
                 "Verus",
-                
-                "Spartan",
-                
-                "UpdatedNCP",
+                "VerusTick",
+                "Vulcan",
+                "NoRules",
                 "BlocksMC",
-
-                "Sparky",
-
-                "VulcanGround",
-                "VulcanVHop",
+                "Karhu",
+                "UNCPLow",
+                "UNCPLow2",
+                "MoonLow",
+                "MoonBHop",
             ]
         }),
-        VulcanTime: Setting.float({
-            name: "Vulcan Time",
-            default: 3.0,
-            min: 0.0,
-            max: 20.0,
+        UNCPLow: Setting.float({
+            name: "UNCP_Height",
+            default: 0.10,
+            min: 0.00,
+            max: 0.10
         }),
-        VulcanYMotion: Setting.float({
-            name: "Vulcan Y-Motion",
-            default: -.20,
-            min: -1.0,
-            max: 1.0,
-        }),
-        VulcanBoostSpeed: Setting.float({
-            name: "VulcanBoostSpeed",
-            default: 1.01,
+        MoonLowSpeed: Setting.float({
+            name: "MoonLowSpeed",
+            default: 1.5,
             min: 1.0,
-            max: 1.1,
-        }),
-        Strafe: Setting.boolean({
-            name: "Strafe",
-            default: true
-        }),
+            max: 1.5
+        })
     }
 }, function(module) {
     module.on("enable", function(){
         
     })
     module.on("disable", function(){
-        mc.timer.timerSpeed = 1
+        setTimer(1)
         timeInAir = 0
     })
     module.on("update", function(){
-        if (module.settings.SpeedMode.get() == "VulcanVHop") {
+        if (module.settings.SpeedMode.get() == "TakaLow") {
+            if (plr.onGround) {
+                setMotionY(.4)
+            }else{
+                setMotionY(-.2)
+                plr.onGround = true
+                speed(1.3)
+            }
+        }else if (module.settings.SpeedMode.get() == "NoRules") {
+            if (plr.onGround) {
+                setMotionY(.3)
+            }else{
+                setMotionY(-1)
+                plr.onGround = true
+                speed(1.75)
+            }
+        }else if (module.settings.SpeedMode.get() == "Verus") {
             if (plr.onGround) {
                 plr.jump()
-                speed(.97)
+                speed(1.45)
+            }else{
+                plr.onGround = true
             }
-            if (timeInAir > module.settings.VulcanTime.get()) {
-                setMotionY(module.settings.VulcanYMotion.get())
-                speed(module.settings.VulcanBoostSpeed.get())
-                timeInAir = 0
-            }
-
-            if (tick > 20) {
-                speed(.99)
+        }else if (module.settings.SpeedMode.get() == "VerusTick") {
+            speed(1.14)
+            if (plr.onGround) {
+                plr.jump()
+                setTimer(20)
+            }else{
+                plr.onGround = true
+                setTimer(.8)
             }
         }else if (module.settings.SpeedMode.get() == "BlocksMC") {
+            speed(1.04)
             setTimer(1.06)
             if (plr.onGround) {
                 plr.jump()
             }else{
                 plr.onGround = true
+            }
+        }else if (module.settings.SpeedMode.get() == "Vulcan") {
+            if (plr.onGround) {
+                plr.jump()
+                speed(.94)
+            }else{
+                if (timeInAir == 3) {
+                    setMotionY(-.3)
+                    timeInAir = 0
+                }
+            }
+        }else if (module.settings.SpeedMode.get() == "UNCPLow") {
+            setTimer(1)
+            if (plr.onGround) {
+                plr.jump()
+            }else if(timeInAir == 3) {
+                setMotionY(-module.settings.UNCPLow.get())
+                setTimer(1.6)
+                plr.onGround = false
+            }else{
+                if (timeInAir > 5) {
+                    //plr.onGround = true
+                }
+            }
+        }else if (module.settings.SpeedMode.get() == "UNCPLow2") {
+            setTimer(1.06)
+            if (plr.onGround) {
+                setMotionY(.14)
+            }else{
+                setMotionY(-.42)
+            }
+        }else if (module.settings.SpeedMode.get() == "MoonLow") {
+            if (plr.onGround) {
+                setMotionY(.42)
+            }else{
+                setMotionY(-.1)
+                plr.onGround = true
+                speed(module.settings.MoonLowSpeed.get())
+            }
+        }else if (module.settings.SpeedMode.get() == "MoonBHop") {
+            if (plr.onGround) {
+                setMotionY(.42)
+                speed(0)
+            }else{
+                if (timeInAir == 2) {
+                    for (var i = 0; i < 20; i ++) {
+                        speed(module.settings.MoonLowSpeed.get())
+                    }
+                }
             }
         }
         
