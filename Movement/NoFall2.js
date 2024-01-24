3d3f28bb9f0f44b13e13711a34c18c9d5ac0d346
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2024-01-24 15:43:17 -0500

    Update NoFall2.js

--- Movement/NoFall2.js
+++ Movement/NoFall2.js
@@ -8,18 +8,18 @@ function setTimer(v) { mc.timer.timerSpeed = v }
 function setYPos(v) { plr.setPosition(plr.posX, plr.posY + v, plr.posZ) }
 var tick = 0
 var tick2 = 0
-var play_client = "net.minecraft.network.play.client"
+var client = "net.minecraft.network.play.client"
 
 var packets = [
-    C0FPacketConfirmTransaction = Java.type(play_client + ".C0FPacketConfirmTransaction"),
-    C04PacketPlayerPosition = Java.type(play_client + ".C03PacketPlayer.C04PacketPlayerPosition"),
-    C18PacketSpectate = Java.type(play_client + ".C18PacketSpectate"),
-    C03PacketPlayer = Java.type(play_client + ".C03PacketPlayer"),
-    C02PacketUseEntity = Java.type(play_client + ".C02PacketUseEntity"),
-    C00PacketKeepAlive = Java.type(play_client + ".C00PacketKeepAlive"),
-    C13PacketPlayerAbilities = Java.type(play_client + ".C13PacketPlayerAbilities"),
-    C09PacketHeldItemChange = Java.type(play_client + ".C09PacketHeldItemChange"),
-    C17PacketCustomPayload = Java.type(play_client + ".C17PacketCustomPayload"),
+    C0FPacketConfirmTransaction = Java.type(client + ".C0FPacketConfirmTransaction"),
+    C04PacketPlayerPosition = Java.type(client + ".C03PacketPlayer.C04PacketPlayerPosition"),
+    C18PacketSpectate = Java.type(client + ".C18PacketSpectate"),
+    C03PacketPlayer = Java.type(client + ".C03PacketPlayer"),
+    C02PacketUseEntity = Java.type(client + ".C02PacketUseEntity"),
+    C00PacketKeepAlive = Java.type(client + ".C00PacketKeepAlive"),
+    C13PacketPlayerAbilities = Java.type(client + ".C13PacketPlayerAbilities"),
+    C09PacketHeldItemChange = Java.type(client + ".C09PacketHeldItemChange"),
+    C17PacketCustomPayload = Java.type(client + ".C17PacketCustomPayload"),
 ]
 
 var NoFall2 = registerScript({
@@ -28,75 +28,41 @@ var NoFall2 = registerScript({
     authors: ["spring67"]
 })
 
+function spoofGround() {
+    sendPacket(new C03PacketPlayer(true))
+}
+
 NoFall2.registerModule({
     name: "NoFall2",
     category: "Fun",
-    description: "craZzy",
-    tag: "blehhh",
+    description: "bypassing nofall",
+    tag: "Vulcan",
     settings: {
         Mode: Setting.list({
             name: "Mode",
-            default: "SpoofGround",
+            default: "Vulcan",
             values: [
-                "SpoofGround",
-                "Tick",
-
-                // vulcan
-                "VulcanSemi",
-                "VulcanSafe",
+                "Vulcan",
+                "Moon",
             ]
-        }),
-        Ticks: Setting.float({
-            name: "Ticks",
-            default: 5,
-            min: 0,
-            max: 20,
         }),
     }
 }, function(module) {
     module.on("enable", function(){
-
+        
     })
     module.on("disable", function(){
         setTimer(1)
+        timeInAir = 0
     })
     module.on("update", function(){
-        if (module.settings.Mode.get() == "SpoofGround") {
-            if (plr.fallDistance > 3) {
-                sendPacket(new C03PacketPlayer(true))
-            }
-
-        }else if (module.settings.Mode.get() == "Tick") {
-            if (plr.fallDistance > 3) {
-                if (tick > module.settings.Ticks.get()) {
-                    sendPacket(new C04PacketPlayerPosition(plr.posX, plr.posY, plr.posZ, false))
-                    tick = 0
-                }
-            }
-
-
-
-        }else if (module.settings.Mode.get() == "VulcanSemi") {
-            if (plr.fallDistance > 3) {
-                if (tick > 2) {
-                    sendPacket(new C03PacketPlayer(true))
-                    sendPacket(new C04PacketPlayerPosition(plr.posX, plr.posY - 100, plr.posZ, true))
-                    setTimer(.05)
-                    setMotionY(-4)
-                    tick = 0
-                }else{
-                    setTimer(.5)
-                    setMotionY(-10)
-                    sendPacket(new C04PacketPlayerPosition(plr.posX, plr.posY - 5, plr.posZ, true))
-                }
-            }
-        }else if (module.settings.Mode.get() == "VulcanSafe") {
+        if (module.settings.Mode.get() == "Vulcan") {
             if (plr.fallDistance > 3) {
                 if (tick > 2) {
                     sendPacket(new C04PacketPlayerPosition(plr.posX, plr.posY - .3, plr.posZ, true))
                     setMotionY(-1)
                     setTimer(.2)
-                    //Chat.print("vulcan aiding")
+                    Chat.print("[LBSCRIPT] vulcan aiding")
                     tick = 0
                 }else{
                     sendPacket(new C04PacketPlayerPosition(plr.posX, plr.posY - .7, plr.posZ, false))
@@ -106,9 +72,20 @@ NoFall2.registerModule({
             }else{
                 setTimer(1)
             }
+        }else if (module.settings.Mode.get() == "Moon") {
+            if (plr.fallDistance > 3) {
+                if (tick > 1) {
+                    spoofGround()
+                    tick = 0
+                }
+            }
         }
-
+        
+        if (!plr.onGround) { timeInAir ++ }else{ timeInAir = 0 }
         tick ++
         module.tag = module.settings.Mode.get()
     })
-})
+    module.on("packet", function(eventPacket){
+        var thePacket = eventPacket.getPacket()
+    })
+});
